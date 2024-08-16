c28c428956baa557e8c586d66bf1ead2398434e5
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-17 00:36:30 +0100

    Start Backend(Architecture projet Spring boot)

---
+++ SOSAuto_BACKEND/src/main/java/com/itsupport/sosauto_backend/SosAutoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itsupport.sosauto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosAutoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosAutoBackendApplication.class, args);
+    }
+
+}
---
+++ SOSAuto_BACKEND/src/test/java/com/itsupport/sosauto_backend/SosAutoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsupport.sosauto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosAutoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
