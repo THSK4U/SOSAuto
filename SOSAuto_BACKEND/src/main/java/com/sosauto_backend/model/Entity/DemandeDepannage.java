99ff422fe8cbd551939473539c2504120f4d8485
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-18 15:03:53 +0100

    Ajouter Entity Demande de Dépannage

---
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Entity/DemandeDepannage.java
@@ -0,0 +1,41 @@
+package com.sosauto_backend.model.Entity;
+
+import com.sosauto_backend.model.Enum.EtatPanne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class DemandeDepannage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int panneid;
+    @Column(updatable = false)
+    private LocalDateTime dateTime;
+    private Double latitude;
+    private Double longitude;
+    @Enumerated(EnumType.STRING)
+    private EtatPanne etat;
+
+    @PrePersist
+    protected void onCreate() {
+        dateTime = LocalDateTime.now();
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "automobiliste_id")
+    private Automobiliste automobiliste;
+
+    @ManyToOne
+    @JoinColumn(name = "mécanicien_id")
+    private Mécanicien mécanicien;
+
+
+}
