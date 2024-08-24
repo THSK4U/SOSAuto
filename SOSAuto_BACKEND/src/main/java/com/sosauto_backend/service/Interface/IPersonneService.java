0533c576af756396ff312df867bd92bab0166387
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-24 12:54:50 +0100

    Commencé Interface Service

---
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/Interface/IPersonneService.java
@@ -0,0 +1,10 @@
+package com.sosauto_backend.service.Interface;
+
+
+import com.sosauto_backend.model.Dto.PersonneDTO;
+
+import java.util.List;
+
+public interface IPersonneService {
+    PersonneDTO create(PersonneDTO personne);
+}
