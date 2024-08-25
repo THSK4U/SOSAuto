bb47fba6707c17de649a59d94e2f43d5892566fc
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-25 21:41:30 +0100

    Implémentation logique des méthodes de service Automobiliste

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/AutomobilisteService.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/AutomobilisteService.java
@@ -1,36 +1,65 @@
 package com.sosauto_backend.service;
 
 import com.sosauto_backend.model.Dto.AutomobilisteDTO;
+import com.sosauto_backend.model.Entity.Automobiliste;
+import com.sosauto_backend.model.Mapper.AutomobilisteMapper;
+import com.sosauto_backend.respository.AutomobilisteRepository;
 import com.sosauto_backend.service.Interface.IAutomobilisteService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AutomobilisteService implements IAutomobilisteService {
 
-    @Override
-    public AutomobilisteDTO creer(AutomobilisteDTO personne) {
-        return null;
-    }
+    private AutomobilisteMapper Mapper;
+
+    private AutomobilisteRepository Repository;
 
     @Override
-    public AutomobilisteDTO mettreAJour(Long id, AutomobilisteDTO DTO) {
-        return null;
-    }
-
-    @Override
-    public void supprimer(Long id) {
-
-    }
-
-    @Override
-    public List<AutomobilisteDTO> voirTous() {
-        return List.of();
+    public AutomobilisteDTO creer(AutomobilisteDTO Automobiliste) {
+        Automobiliste Automobilistes = Mapper.toEntity(Automobiliste);
+        Automobiliste saved = Repository.save(Automobilistes);
+        return Mapper.toDTO(saved);
     }
 
     @Override
     public AutomobilisteDTO getById(Long id) {
-        return null;
+        Optional<Automobiliste> automobiliste = Repository.findById(id);
+        return automobiliste.map(Mapper::toDTO).orElse(null);
     }
+
+    @Override
+    public List<AutomobilisteDTO> voirTous() {
+        List<Automobiliste> automobiliste = Repository.findAll();
+        return automobiliste.stream()
+                .map(Mapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AutomobilisteDTO mettreAJour(Long id, AutomobilisteDTO DTO) {
+        Optional<Automobiliste> optional = Repository.findById(id);
+        if (optional.isPresent()) {
+            Automobiliste automobiliste = optional.get();
+            automobiliste.setEmail(DTO.getEmail());
+            automobiliste.setNom(DTO.getNom());
+            automobiliste.setPrenom(DTO.getPrenom());
+            automobiliste.setPassword(DTO.getPassword());
+
+
+            Automobiliste updated = Repository.save(automobiliste);
+            return Mapper.toDTO(updated);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void supprimer(Long id) {
+        Repository.deleteById(id);
+    }
+
 }
