a93890a862fa39b5efcbcd7a2ae2afe0b7217e2a
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-25 03:16:58 +0100

    Modifier Type Repository

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/AdministrateurRepository.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/AdministrateurRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdministrateurRepository extends JpaRepository<Administrateur, Integer> {
+public interface AdministrateurRepository extends JpaRepository<Administrateur, Long> {
 
 }
--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/PersonneRepository.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/PersonneRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonneRepository extends JpaRepository<Personne, Integer> {
+public interface PersonneRepository extends JpaRepository<Personne, Long> {
 
 }
