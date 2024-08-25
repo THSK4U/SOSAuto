3bfd15e38aeb69432b973cd5149ee1f8fbe6f22d
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-25 05:00:44 +0100

    Renamed Interface Demande

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Mapper/DemandeDepannageMapper.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Mapper/DemandeDepannageMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface DamandeDepannageMapper {
+public interface DemandeDepannageMapper {
 
     DemandeDepannage toEntity(DemandeDepannageDTO DTO);
     DemandeDepannageDTO toDTO(DemandeDepannage entity);
