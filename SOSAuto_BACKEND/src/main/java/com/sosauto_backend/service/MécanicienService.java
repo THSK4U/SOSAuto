1ba8a9b0145d9da24eebc264e353ec43e3cbc050
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-25 21:42:55 +0100

    Implémentation logique des méthodes de service Mécanicien

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/MécanicienService.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/MécanicienService.java
@@ -1,36 +1,66 @@
 package com.sosauto_backend.service;
 
+
 import com.sosauto_backend.model.Dto.MécanicienDTO;
+import com.sosauto_backend.model.Entity.Mécanicien;
+import com.sosauto_backend.model.Mapper.MécanicienMapper;
+import com.sosauto_backend.respository.MécanicienRepository;
 import com.sosauto_backend.service.Interface.IMécanicienService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MécanicienService implements IMécanicienService {
 
-    @Override
-    public MécanicienDTO creer(MécanicienDTO personne) {
-        return null;
-    }
+    private MécanicienMapper Mapper;
+
+    private MécanicienRepository Repository;
 
     @Override
-    public MécanicienDTO mettreAJour(Long id, MécanicienDTO DTO) {
-        return null;
-    }
-
-    @Override
-    public void supprimer(Long id) {
-
-    }
-
-    @Override
-    public List<MécanicienDTO> voirTous() {
-        return List.of();
+    public MécanicienDTO creer(MécanicienDTO Mécanicien) {
+        Mécanicien mécaniciens = Mapper.toEntity(Mécanicien);
+        Mécanicien saved = Repository.save(mécaniciens);
+        return Mapper.toDTO(saved);
     }
 
     @Override
     public MécanicienDTO getById(Long id) {
-        return null;
+        Optional<Mécanicien> mécanicien = Repository.findById(id);
+        return mécanicien.map(Mapper::toDTO).orElse(null);
     }
+
+    @Override
+    public List<MécanicienDTO> voirTous() {
+        List<Mécanicien> mécanicien = Repository.findAll();
+        return mécanicien.stream()
+                .map(Mapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public MécanicienDTO mettreAJour(Long id, MécanicienDTO DTO) {
+        Optional<Mécanicien> optional = Repository.findById(id);
+        if (optional.isPresent()) {
+            Mécanicien mécanicien = optional.get();
+            mécanicien.setEmail(DTO.getEmail());
+            mécanicien.setNom(DTO.getNom());
+            mécanicien.setPrenom(DTO.getPrenom());
+            mécanicien.setPassword(DTO.getPassword());
+
+
+            Mécanicien updated = Repository.save(mécanicien);
+            return Mapper.toDTO(updated);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void supprimer(Long id) {
+        Repository.deleteById(id);
+    }
+
 }
