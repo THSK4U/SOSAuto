d5c1f9b65de59da38f9bd9c52de3cc2b1175e8a0
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-25 21:43:24 +0100

    Implémentation logique des méthodes de service Véhicule

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/VéhiculeService.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/VéhiculeService.java
@@ -1,36 +1,68 @@
 package com.sosauto_backend.service;
 
+
+
 import com.sosauto_backend.model.Dto.VéhiculeDTO;
+import com.sosauto_backend.model.Entity.Véhicule;
+import com.sosauto_backend.model.Mapper.véhiculeMapper;
+import com.sosauto_backend.respository.VéhiculeRepository;
 import com.sosauto_backend.service.Interface.IVéhiculeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class VéhiculeService implements IVéhiculeService {
 
-    @Override
-    public VéhiculeDTO creer(VéhiculeDTO personne) {
-        return null;
-    }
+
+    private véhiculeMapper Mapper;
+
+    private VéhiculeRepository Repository;
 
     @Override
-    public VéhiculeDTO mettreAJour(Long id, VéhiculeDTO DTO) {
-        return null;
-    }
-
-    @Override
-    public void supprimer(Long id) {
-
-    }
-
-    @Override
-    public List<VéhiculeDTO> voirTous() {
-        return List.of();
+    public VéhiculeDTO creer(VéhiculeDTO Véhicule) {
+        Véhicule véhicule = Mapper.toEntity(Véhicule);
+        Véhicule saved = Repository.save(véhicule);
+        return Mapper.toDTO(saved);
     }
 
     @Override
     public VéhiculeDTO getById(Long id) {
-        return null;
+        Optional<Véhicule> Véhicule = Repository.findById(id);
+        return Véhicule.map(Mapper::toDTO).orElse(null);
     }
+
+    @Override
+    public List<VéhiculeDTO> voirTous() {
+        List<Véhicule> Véhicule = Repository.findAll();
+        return Véhicule.stream()
+                .map(Mapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public VéhiculeDTO mettreAJour(Long id, VéhiculeDTO DTO) {
+        Optional<Véhicule> optional = Repository.findById(id);
+        if (optional.isPresent()) {
+            Véhicule Véhicule = optional.get();
+            Véhicule.setAnnée(DTO.getAnnée());
+            Véhicule.setModele(DTO.getModele());
+            Véhicule.setCouleur(DTO.getCouleur());
+            Véhicule.setMarque(DTO.getMarque());
+            Véhicule.setMatricule(DTO.getMatricule());
+
+            Véhicule updated = Repository.save(Véhicule);
+            return Mapper.toDTO(updated);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void supprimer(Long id) {
+        Repository.deleteById(id);
+    }
+
 }
