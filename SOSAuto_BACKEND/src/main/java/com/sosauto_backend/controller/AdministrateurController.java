4ad11ad84d848fa2321c3ee51eb6eeccd7b601a2
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-26 23:12:23 +0100

    Implementation Administrateur Controller

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/controller/AdministrateurController.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/controller/AdministrateurController.java
@@ -1,4 +1,29 @@
 package com.sosauto_backend.controller;
 
-public class AdministrateurController {
+import com.sosauto_backend.controller.api.IAdministrateurApi;
+import com.sosauto_backend.model.Dto.AdministrateurDTO;
+import com.sosauto_backend.service.Interface.IAdministrateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdministrateurController implements IAdministrateurApi {
+
+    @Autowired
+    private IAdministrateurService Service;
+
+    @Override
+    public AdministrateurDTO createAdministrateur(AdministrateurDTO administrateurDTO) {
+        return Service.creer(administrateurDTO);
+    }
+
+    @Override
+    public void deleteAdministrateur(Long id) {
+        Service.supprimer(id);
+    }
+
+    @Override
+    public AdministrateurDTO updateAdministrateur(Long id, AdministrateurDTO administrateurDTO) {
+        return Service.mettreAJour(id, administrateurDTO);
+    }
 }
