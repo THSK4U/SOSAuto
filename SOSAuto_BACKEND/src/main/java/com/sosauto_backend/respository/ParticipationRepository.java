22dfdc25fe4f95fa4fe81b868150624c49f2e8ab
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-27 23:08:22 +0100

    Participation Repository

---
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/ParticipationRepository.java
@@ -0,0 +1,9 @@
+package com.sosauto_backend.respository;
+
+import com.sosauto_backend.model.Entity.Participation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipationRepository extends JpaRepository<Participation, Long>{
+}
