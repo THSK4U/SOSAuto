3d1d576c0b5caffdf1a0598bb98f327921366a1b
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-27 23:08:34 +0100

    interface Participation Service

---
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/service/Interface/IParticipationService.java
@@ -0,0 +1,24 @@
+package com.sosauto_backend.service.Interface;
+
+
+import com.sosauto_backend.model.Dto.ParticipationDTO;
+
+import java.util.List;
+
+public interface IParticipationService {
+    //Creer
+    ParticipationDTO creer(ParticipationDTO personne);
+
+    //mettre A Jour
+    ParticipationDTO mettreAJour(Long id, ParticipationDTO DTO);
+
+    //supprimer
+    void supprimer(Long id);
+
+    //consulter Tous
+    List<ParticipationDTO> voirTous();
+
+    //By ID
+    ParticipationDTO getById(Long id);
+
+}
