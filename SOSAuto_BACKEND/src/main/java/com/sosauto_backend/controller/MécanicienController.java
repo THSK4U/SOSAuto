7315704208393be4305ad38f673a7cb6f6c743bc
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-30 03:36:06 +0100

    Mécanicien Controller

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/controller/MécanicienController.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/controller/MécanicienController.java
@@ -1,4 +1,43 @@
 package com.sosauto_backend.controller;
 
-public class MécanicienController {
+
+import com.sosauto_backend.controller.api.IMécanicienApi;
+import com.sosauto_backend.model.Dto.MécanicienDTO;
+import com.sosauto_backend.service.Interface.IMécanicienService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MécanicienController implements IMécanicienApi {
+
+    @Autowired
+    private IMécanicienService service;
+
+    @Override
+    public MécanicienDTO creerMécanicien(MécanicienDTO Mécanicien) {
+        return service.creer(Mécanicien);
+    }
+
+    @Override
+    public void supprimerMécanicien(Long id) {
+        service.supprimer(id);
+    }
+
+    @Override
+    public List<MécanicienDTO> getAllMécanicien() {
+        return service.voirTous();
+    }
+
+    @Override
+    public MécanicienDTO mettreAjourMécanicien(Long id, MécanicienDTO mécanicien) {
+        return service.mettreAJour(id,mécanicien);
+    }
+
+    @Override
+    public MécanicienDTO getMécanicienById(Long id) {
+        return service.getById(id);
+    }
 }
+
