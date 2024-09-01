0314dcc0507391c13accd8a2bb3176f58c222102
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-01 23:52:04 +0100

    Déplacer les fichiers assets

--- SOSAuto_FRONTEND/src/app/app.module.ts
+++ SOSAuto_FRONTEND/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MecaniciensSideComponent } from './mecaniciens-side/mecaniciens-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MecaniciensSideComponent
   ],
   imports: [
     BrowserModule,
---
+++ SOSAuto_FRONTEND/src/app/mecaniciens-side/mecaniciens-side.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mecaniciens-side',
+  templateUrl: './mecaniciens-side.component.html',
+  styleUrls: ['./mecaniciens-side.component.scss']
+})
+export class MecaniciensSideComponent {
+
+}
