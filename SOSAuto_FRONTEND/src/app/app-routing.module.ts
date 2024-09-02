b4a526151535a38c699af48997e237ddee91547e
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-02 22:14:54 +0100

    Quelques Modification

--- SOSAuto_FRONTEND/src/app/app-routing.module.ts
+++ SOSAuto_FRONTEND/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {MapComponent} from "./mechanic/map/map.component";
 
 const routes: Routes = [
   {
-    path: '',component: AppComponent
-  }
+    path: '',component: AppComponent,
+  },
+  { path: 'Automobiliste', loadChildren: () => import('./motorist/motorist.module').then(m => m.MotoristModule) },
+  { path: 'Mecanicien', loadChildren: () => import('./mechanic/mechanic.module').then(m => m.MechanicModule) },
+  { path: 'Admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  {path: 'MAP', component: MapComponent},
+  { path: '**', component: AppComponent}
 ];
 
 @NgModule({
--- SOSAuto_FRONTEND/src/app/app.module.ts
+++ SOSAuto_FRONTEND/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MecaniciensSideComponent } from './mecaniciens-side/mecaniciens-side.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MecaniciensSideComponent
   ],
   imports: [
     BrowserModule,
