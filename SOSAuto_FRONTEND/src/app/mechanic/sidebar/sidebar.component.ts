d38fd979ee9c04d852f180a4222f2d068d6eb7ce
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-03 02:10:21 +0100

    Afficher List Des Demande de depannage Sur sidebar

--- SOSAuto_FRONTEND/src/app/mechanic/sidebar/sidebar.component.ts
+++ SOSAuto_FRONTEND/src/app/mechanic/sidebar/sidebar.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Mecanicien} from "../../services/models/mecanicien";
+import {ApiService} from "../../services/services/api.service";
+import {Router} from "@angular/router";
+import {DemandeDepannageDto} from "../../services/models/demande-depannage-dto";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  Demandes: DemandeDepannageDto[] = [];
+
+  constructor(
+    private service: ApiService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllDemande();
+  }
+
+  private findAllDemande() {
+    this.service.getAllDemande()
+      .subscribe({
+        next: (demande) => {
+          this.Demandes = demande;
+        },
+        error: (err) => {
+          console.error('Error fetching demands', err);
+        }
+      });
+  }
 }
