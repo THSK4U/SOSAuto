aae8ca572942e2c2b29fe3563efc8e2308bda540
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-03 02:08:56 +0100

    HttpClient sur app-module

--- SOSAuto_FRONTEND/src/app/app.module.ts
+++ SOSAuto_FRONTEND/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -9,9 +10,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
