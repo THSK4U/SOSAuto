ccb3e1f75c1f809b6178a518ed20fb6e5ff3394c
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-07 19:30:48 +0100

    Page Automobiliste avec List et Map

--- SOSAuto_FRONTEND/src/app/motorist/motorist.module.ts
+++ SOSAuto_FRONTEND/src/app/motorist/motorist.module.ts
@@ -4,20 +4,22 @@ import { CommonModule } from '@angular/common';
 import { MotoristRoutingModule } from './motorist-routing.module';
 import { MotoristComponent } from './motorist.component';
 import { MapMotoristComponent } from './map-motorist/map-motorist.component';
-import {MechanicModule} from "../mechanic/mechanic.module";
 import { ListSuggestionsComponent } from './list-suggestions/list-suggestions.component';
+import {NavbarComponent} from "./navbar/navbar.component";
+import { CreatdemandeComponent } from './creatdemande/creatdemande.component';
 
 
 @NgModule({
   declarations: [
     MotoristComponent,
     MapMotoristComponent,
-    ListSuggestionsComponent
+    ListSuggestionsComponent,
+    NavbarComponent,
+    CreatdemandeComponent
   ],
     imports: [
         CommonModule,
         MotoristRoutingModule,
-        MechanicModule
     ]
 })
 export class MotoristModule { }
