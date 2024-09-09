80ee11e3fdde4532942e0795474191534000a103
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-09 14:10:01 +0100

    implements UserDetails sur Personne Entity

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Entity/Personne.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Entity/Personne.java
@@ -5,6 +5,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -12,7 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name ="type")
-public class Personne {
+public class Personne implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long personneid;
@@ -24,4 +30,35 @@ public class Personne {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public String getUsername() {
+        return numTelephone;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        System.out.println("role "+role.name());
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
