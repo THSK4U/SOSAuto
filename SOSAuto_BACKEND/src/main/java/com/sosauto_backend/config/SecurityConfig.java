2ef9bef69acb3275f7a53a7e32a9a799676d124a
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-09 14:27:42 +0100

    Ajouter beans PasswordEncoder et AuthenticationManager

--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/config/SecurityConfig.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/config/SecurityConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,13 +27,22 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers("/api/**").permitAll();
-                    auth.anyRequest().authenticated();})
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
+                    auth.requestMatchers("/login","/**").permitAll();
+//                    auth.anyRequest().authenticated();
+                  })
+//                .oauth2Login(withDefaults())
                 .build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
     @Bean
     public WebMvcConfigurer corsConfig() {
         return new WebMvcConfigurer() {
