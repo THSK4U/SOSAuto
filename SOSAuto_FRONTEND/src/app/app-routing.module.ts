66a58c64b777fe2879ba2cd7d8527400691cb955
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-16 01:47:06 +0100

    Ajouter Route login

--- SOSAuto_FRONTEND/src/app/app-routing.module.ts
+++ SOSAuto_FRONTEND/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import {MapComponent} from "./mechanic/map/map.component";
+import {SigninComponent} from "./authentication/signin/signin.component";
 
 const routes: Routes = [
   {
@@ -10,8 +11,8 @@ const routes: Routes = [
   { path: 'Automobiliste', loadChildren: () => import('./motorist/motorist.module').then(m => m.MotoristModule) },
   { path: 'Mecanicien', loadChildren: () => import('./mechanic/mechanic.module').then(m => m.MechanicModule) },
   { path: 'Admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'Login', component: SigninComponent},
   {path: 'MAP', component: MapComponent},
-  { path: '**', component: AppComponent}
 ];
 
 @NgModule({
