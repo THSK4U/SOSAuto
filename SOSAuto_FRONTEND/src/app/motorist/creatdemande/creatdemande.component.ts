6193cbc9abe4eee3153669cdfa56ff84e1b49b3d
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-16 01:46:18 +0100

    Afficher List des pannes sur Form Creation De Demande

--- SOSAuto_FRONTEND/src/app/motorist/creatdemande/creatdemande.component.ts
+++ SOSAuto_FRONTEND/src/app/motorist/creatdemande/creatdemande.component.ts
@@ -1,27 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ApiService } from '../../services/services/api.service';
 import { CreerDemande$Params } from "../../services/fn/operations/creer-demande";
 import { StrictHttpResponse } from "../../services/strict-http-response";
 import { DemandeDepannageDto } from "../../services/models/demande-depannage-dto";
 import { SharedDataService } from "../../services/services/share-data.service";
+import {PanneDto} from "../../services/models/panne-dto";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-creatdemande',
   templateUrl: './creatdemande.component.html',
   styleUrls: ['./creatdemande.component.scss']
 })
-export class CreatdemandeComponent {
+export class CreatdemandeComponent implements OnInit{
 
+  pannes: PanneDto[] = [];
   description: string = '';
   automobiliste: { personneid: number } = { personneid: 0 };
   panne: { panneid: number } = { panneid: 0 };
 
   constructor(private service: ApiService,
               private sharedDataService: SharedDataService,
+              private tokenService: TokenService
+
   ) {}
+
+  ngOnInit(): void {
+   this.getPannes()
+  }
+
+  getPannes() {
+    this.service.getAllPanne().subscribe((data: PanneDto[]) => {
+      this.pannes = data;
+    });
+  }
 
   onSubmit(): void {
     const location = this.sharedDataService.getUserLocation();
+    const Id = this.tokenService.getIDFromJwt(this.tokenService.token)
 
     if (location && location.lat && location.lng) {
       const requestParams: CreerDemande$Params = {
@@ -29,19 +45,15 @@ export class CreatdemandeComponent {
           latitude: location.lat,
           longitude: location.lng,
           description: this.description,
-          automobiliste: {
-            personneid: this.automobiliste?.personneid
-          },
+          automobiliste: {personneid: Id},
           panne: {
             panneid: this.panne?.panneid
-          }
-        }
-      };
+          }}};
 
       this.service.creerDemande$Response(requestParams).subscribe(
         (response: StrictHttpResponse<DemandeDepannageDto>) => {
           console.log('Demande créée avec succès', response.body);
-
+          window.location.reload();
         },
         (error) => {
           console.error('La création de la demande a échoué:', error);
@@ -52,4 +64,5 @@ export class CreatdemandeComponent {
       alert('Impossible de récupérer la localisation de l\'utilisateur. Veuillez vous assurer que les services de localisation sont activés.');
     }
   }
+
 }
