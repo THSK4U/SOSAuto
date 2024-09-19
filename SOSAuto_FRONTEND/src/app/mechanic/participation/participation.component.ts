31bb87ca769311aa9b77a4785364a451769ca307
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-19 02:17:31 +0100

    Ajouter fonctionnalité d'annulation de participation

--- SOSAuto_FRONTEND/src/app/mechanic/participation/participation.component.ts
+++ SOSAuto_FRONTEND/src/app/mechanic/participation/participation.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../../services/services/api.service";
 import {HttpClient} from "@angular/common/http";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {TokenService} from "../../services/token/token.service";
 import {ParticipationDto} from "../../services/models/participation-dto";
 import {CreerDemande$Params} from "../../services/fn/operations/creer-demande";
 import {CreerParticipation$Params} from "../../services/fn/operations/creer-participation";
+import {AnnulerParticipation$Params} from "../../services/fn/operations/annuler-participation";
 
 @Component({
   selector: 'app-participation',
@@ -15,10 +16,12 @@ import {CreerParticipation$Params} from "../../services/fn/operations/creer-part
 export class ParticipationComponent implements OnInit{
   demandeid: number = 0;
   participation: ParticipationDto[] = [];
+  createdParticipationId: number = 0;
   constructor(
     private service: ApiService,
     private http: HttpClient,
     private route: ActivatedRoute,
+    private router: Router,
     private tokenService: TokenService,
     ){}
 
@@ -37,8 +40,8 @@ export class ParticipationComponent implements OnInit{
     console.log(requestParams.body);
     this.service.creerParticipation$Response(requestParams).subscribe(
       (response) => {
-        const createdParticipationId = response.body.id;
-        console.log('Participation créée avec succès', createdParticipationId);
+        this.createdParticipationId = response.body.id!;
+        console.log('Participation créée avec succès', this.createdParticipationId);
       },(error) => {
       console.error('La création de la demande a échoué:', error);
     }
@@ -46,6 +49,19 @@ export class ParticipationComponent implements OnInit{
   }
 
   cancelRequest() {
+    const requestParams: AnnulerParticipation$Params = {
+            id: this.createdParticipationId
+    }
+      this.service.annulerParticipation$Response(requestParams).subscribe(
+        (response) => {
+          console.log('Participation annulée avec succès');
+          this.router.navigate(['/mecanicien']);
+        },
+        (error) => {
+          console.log("Participation non annulée", requestParams);
+          console.error('L\'annulation de la participation a échoué:', error);
+        }
+      );
 
   }
 }
