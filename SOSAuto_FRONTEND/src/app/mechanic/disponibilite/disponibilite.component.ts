104357ef7171292662500e06636628b7bf50039f
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-19 03:42:38 +0100

    Ajouter la gestion de la disponibilité du mécanicien dans DisponibiliteComponent

--- SOSAuto_FRONTEND/src/app/mechanic/disponibilite/disponibilite.component.ts
+++ SOSAuto_FRONTEND/src/app/mechanic/disponibilite/disponibilite.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../../services/services/api.service";
+import {Router} from "@angular/router";
+import {TokenService} from "../../services/token/token.service";
+import {MettreAjourDisponibilite$Params} from "../../services/fn/operations/mettre-ajour-disponibilite";
+import {MecanicienDto} from "../../services/models/mecanicien-dto";
 
 @Component({
   selector: 'app-disponibilite',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./disponibilite.component.scss']
 })
 export class DisponibiliteComponent {
+  mecanicien: MecanicienDto = {};
 
+  constructor(
+    private router: Router,
+    private Service: ApiService,
+    private Token : TokenService
+  ) {
+  }
+
+
+  toggleDisponibilite(): void {
+    this.mecanicien.disponible = this.mecanicien.disponible === 'DISPONIBLE' ? 'INDISPONIBLE' : 'DISPONIBLE';
+    this.updateDisponibilite();
+  }
+
+  private updateDisponibilite(): void {
+    const Id = this.Token.getIDFromLocalJwt();
+    const requestParams: MettreAjourDisponibilite$Params = {
+      id: Id,
+      body: {
+        disponible: this.mecanicien.disponible
+      }
+    };
+
+    this.Service.mettreAjourDisponibilite$Response(requestParams).subscribe(
+      (data: any) => {
+        console.log('تم تحديث الحالة بنجاح:', data);
+      },
+      (error) => {
+        console.error('خطأ في تحديث الحالة:', error);
+      }
+    );
+  }
 }
