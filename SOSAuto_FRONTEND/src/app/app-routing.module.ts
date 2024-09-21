393068d27c21b23c47611c52ff041be97f20fae6
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-21 02:47:31 +0100

    Ajouter HomeComponent et mettre à jour les routes pour inclure les chemins 'home' et 'register'

--- SOSAuto_FRONTEND/src/app/app-routing.module.ts
+++ SOSAuto_FRONTEND/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import {MapComponent} from "./mechanic/map/map.component";
 import {SigninComponent} from "./authentication/signin/signin.component";
+import {SignupComponent} from "./authentication/signup/signup.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {
-    path: '',component: AppComponent,
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
   { path: 'automobiliste', loadChildren: () => import('./motorist/motorist.module').then(m => m.MotoristModule) },
   { path: 'mecanicien', loadChildren: () => import('./mechanic/mechanic.module').then(m => m.MechanicModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'login', component: SigninComponent},
+  { path: 'register', component: SignupComponent},
   {path: 'MAP', component: MapComponent},
 ];
 
