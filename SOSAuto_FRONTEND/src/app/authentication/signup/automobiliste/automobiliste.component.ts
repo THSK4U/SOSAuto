cbce2a4b71606fbd2873db7553f90442fda169d5
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-23 11:54:38 +0100

    Ajouter le téléchargement de la carte d'id dans AutomobilisteComponent

--- SOSAuto_FRONTEND/src/app/authentication/signup/automobiliste/automobiliste.component.ts
+++ SOSAuto_FRONTEND/src/app/authentication/signup/automobiliste/automobiliste.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UploadImage} from "../../../services/services/UploadImage";
+import {ApiService} from "../../../services/services/api.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-automobiliste',
@@ -10,8 +13,12 @@ export class AutomobilisteComponent implements OnInit {
   registerForm!: FormGroup;
   hidePassword = true;
   errorMsg: Array<string> = [];
+  imagePreviewUrl: string | null = null;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+              private uploadService: UploadImage,
+              private Service: ApiService,
+              private router: Router) {}
 
   ngOnInit() {
     this.initForm();
@@ -24,16 +31,43 @@ export class AutomobilisteComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       numTelephone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      nationalIdCardUrl: [''],
     });
   }
 
 
-  onSubmit() {
+  async onSubmit() {
     if (this.registerForm.valid) {
-      this.errorMsg = [];
-      console.log(this.registerForm.value);
+      const formData = this.registerForm.value;
+      console.log('Form Data:', this.registerForm.value);
+
+      formData.nationalIdCardUrl = await this.uploadService.uploadImageToCloudinary(formData.nationalIdCardUrl);
+
+      console.log('Form Data after:', formData);
+
+      this.Service.creerAutomobiliste$Response({
+        body: formData
+      }).subscribe(
+        data => {
+          console.log('Automobiliste created successfully:', data);
+          this.router.navigate(['/login']);
+        }
+      )
+
+    }
+  }
+
+  onFileSelected(event: any, controlName: string): void {
+    const file = event.target.files[0];
+    if (file && file.type.match(/image\/*/) != null) {
+      const previewUrl = URL.createObjectURL(file);
+      this.registerForm.patchValue({
+        [controlName]: file
+      });
+      this.registerForm.get(controlName)?.markAsTouched();
+        this.imagePreviewUrl = previewUrl;
     } else {
-      this.errorMsg.push('Veuillez remplir tous les champs requis.');
+      alert('Veuillez sélectionner une image valide');
     }
   }
 }
