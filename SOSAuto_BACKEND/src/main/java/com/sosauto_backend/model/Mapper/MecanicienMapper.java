dc1751dff9ee23f7b1079337a921eff16bc29020
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-30 15:14:33 +0100

    Corriger les noms de classes et de variables

---
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/model/Mapper/MecanicienMapper.java
@@ -0,0 +1,16 @@
+package com.sosauto_backend.model.Mapper;
+
+import com.sosauto_backend.model.Dto.MecanicienDTO;
+import com.sosauto_backend.model.Entity.Mecanicien;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MecanicienMapper {
+
+    Mecanicien toEntity(MecanicienDTO DTO);
+    MecanicienDTO toDTO(Mecanicien entity);
+    List<MecanicienDTO> toDTOList(List<Mecanicien> All);
+    List<Mecanicien> toEntityList(List<MecanicienDTO> AllDTOs);
+}
--- SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/MecanicienRepository.java
+++ SOSAuto_BACKEND/src/main/java/com/sosauto_backend/respository/MecanicienRepository.java
@@ -1,7 +1,7 @@
 package com.sosauto_backend.respository;
 
 
-import com.sosauto_backend.model.Entity.Mécanicien;
+import com.sosauto_backend.model.Entity.Mecanicien;
 import com.sosauto_backend.model.Enum.Disponibilite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,10 +10,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MécanicienRepository extends JpaRepository<Mécanicien,Long> {
+public interface MecanicienRepository extends JpaRepository<Mecanicien,Long> {
 
-    List<Mécanicien> getAllByDisponible(Disponibilite disponible);
+    List<Mecanicien> getAllByDisponible(Disponibilite disponible);
 
-    Optional<Mécanicien> findBynumTelephone(String numTelephone);
+    Optional<Mecanicien> findBynumTelephone(String numTelephone);
 
 }
