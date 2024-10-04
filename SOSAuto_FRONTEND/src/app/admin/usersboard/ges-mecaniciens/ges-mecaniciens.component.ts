afb61dfb85f15aa0b0f13d697f973245cc59415a
TAHA SELLAK <sellaktaha1@gmail.com>
2024-10-04 11:38:17 +0100

    Ajouter la fonctionnalité de suppression avec confirmation pour la gestion des users

--- SOSAuto_FRONTEND/src/app/admin/usersboard/ges-mecaniciens/ges-mecaniciens.component.ts
+++ SOSAuto_FRONTEND/src/app/admin/usersboard/ges-mecaniciens/ges-mecaniciens.component.ts
@@ -1,7 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Automobiliste} from "../../../services/models/automobiliste";
 import {Mecanicien} from "../../../services/models/mecanicien";
 import {ApiService} from "../../../services/services/api.service";
+import {PageEvent} from "@angular/material/paginator";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import {SupprimerMecanicien$Params} from "../../../services/fn/operations/supprimer-mecanicien";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-ges-mecaniciens',
@@ -10,22 +14,69 @@ import {ApiService} from "../../../services/services/api.service";
 })
 export class GesMecaniciensComponent  implements OnInit {
   mecaniciens: Mecanicien[] = [];
+  displayedMecanicien: Mecanicien[] = [];
+  pageSize = 7;
+  pageIndex = 0;
+  length = 0;
+  @ViewChild('confirmDeleteTemplate') confirmDeleteTemplate!: TemplateRef<any>;
+  dialogRef!: MatDialogRef<any>;
+  constructor(private Service: ApiService,
+              private toastr: ToastrService,
+              private dialog: MatDialog,
 
-  constructor(private Service: ApiService) {}
+  ) {}
 
   ngOnInit(): void {
     this.getMecaniciens();
   }
-
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updateDisplayedAutomobilistes();
+  }
 
   private getMecaniciens(): void {
     this.Service.getAllMecanicien().subscribe(
       (list) => {
         this.mecaniciens = list;
+        this.length = list.length;
+        this.updateDisplayedAutomobilistes();
       },
       (error) => {
         console.error('Error loading mecaniciens:', error);
       }
+    );
+  }
+
+  private updateDisplayedAutomobilistes() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedMecanicien = this.mecaniciens.slice(startIndex, endIndex);
+  }
+  openDialog(personneid?: number): void {
+    this.dialogRef = this.dialog.open(this.confirmDeleteTemplate, {
+      width: '400px',
+    });
+
+    this.dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.DeleteUser(personneid);
+      }
+    });
+  }
+  DeleteUser(personneid?: number) {
+    const requestParams:  SupprimerMecanicien$Params = {
+      id: personneid
+    }
+    this.Service.supprimerMecanicien$Response(requestParams).subscribe(
+        (response) => {
+          this.toastr.success('Mecanicien supprimé avec succès', 'succès!');
+          this.getMecaniciens();
+        },
+        (error) => {
+          this.toastr.error('Erreur lors de la suppression du Mecanicien', 'échoué!');
+          console.error('Erreur lors de la suppression du Mecanicien:', error);
+        }
     );
   }
 }
--- SOSAuto_FRONTEND/src/app/admin/usersboard/ges-motorist/ges-motorist.component.ts
+++ SOSAuto_FRONTEND/src/app/admin/usersboard/ges-motorist/ges-motorist.component.ts
@@ -1,7 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Automobiliste} from "../../../services/models/automobiliste";
 import {Mecanicien} from "../../../services/models/mecanicien";
 import {ApiService} from "../../../services/services/api.service";
+import {PageEvent} from "@angular/material/paginator";
+import {ToastrService} from "ngx-toastr";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {SupprimerMecanicien$Params} from "../../../services/fn/operations/supprimer-mecanicien";
+import {SupprimerAutomobiliste$Params} from "../../../services/fn/operations/supprimer-automobiliste";
 
 @Component({
   selector: 'app-ges-motorist',
@@ -10,17 +15,30 @@ import {ApiService} from "../../../services/services/api.service";
 })
 export class GesMotoristComponent  implements OnInit {
   automobilistes: Automobiliste[] = [];
-
-  constructor(private Service: ApiService) {}
+  displayedAutomobilistes: Automobiliste[] = [];
+  pageSize = 7;
+  pageIndex = 0;
+  length = 0;
+  @ViewChild('confirmDeleteTemplate') confirmDeleteTemplate!: TemplateRef<any>;
+  dialogRef!: MatDialogRef<any>;
+  constructor(private Service: ApiService,
+              private toastr: ToastrService,
+              private dialog: MatDialog,) {}
 
   ngOnInit(): void {
     this.getUsers();
   }
-
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updateDisplayedAutomobilistes();
+  }
   private getUsers(): void {
     this.Service.getAllAutomobiliste().subscribe(
       (data) => {
         this.automobilistes = data;
+        this.length = data.length;
+        this.updateDisplayedAutomobilistes();
       },
       (error) => {
         console.error('Error loading users:', error);
@@ -28,4 +46,35 @@ export class GesMotoristComponent  implements OnInit {
     );
   }
 
+  private updateDisplayedAutomobilistes() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedAutomobilistes = this.automobilistes.slice(startIndex, endIndex);
+  }
+  openDialog(personneid?: number): void {
+    this.dialogRef = this.dialog.open(this.confirmDeleteTemplate, {
+      width: '400px',
+    });
+
+    this.dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.DeleteUser(personneid);
+      }
+    });
+  }
+  DeleteUser(personneid?: number) {
+    const requestParams:  SupprimerAutomobiliste$Params = {
+      id: personneid
+    }
+    this.Service.supprimerAutomobiliste$Response(requestParams).subscribe(
+      (response) => {
+        this.toastr.success('Automobiliste supprimé avec succès', 'succès!');
+        this.getUsers();
+      },
+      (error) => {
+        this.toastr.error('Erreur lors de la suppression du Automobilistes', 'échoué!');
+        console.error('Erreur lors de la suppression du Automobilistes:', error);
+      }
+    );
+  }
 }
